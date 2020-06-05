333af00f5c8641ae72a189f7a876702c5f52579a
Matthew Dewhirst <matthew.dewhirst@skybettingandgaming.com>
2020-06-05 14:15:12 +0100

    tidy up main function and added script to create key file which is for the steam dev api key

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,3 @@
-//http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=  &steamids=
-
 import { getUserFromDB } from "./data-helpers/users-helper";
 import {
   addUsersGamesToDB,
@@ -12,10 +10,11 @@ import {
   getGameCategoriesFromDB,
   UpdateGameInDB
 } from "./data-helpers/games-helper";
-
-const getGamesSteamUrl =
-  "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/";
-const getAppDetailsSteamUrl = "https://store.steampowered.com/api/appdetails";
+import {
+  getAppDetailsSteamUrl,
+  getGamesSteamUrl,
+  multiPlayerCategories
+} from "./steam-api-helpers/consts";
 
 const getUser = async (steamId: string) => {
   //try to get user from db
@@ -37,6 +36,7 @@ const checkUserGames = async (steamId: string) => {
     return true;
   }
 
+  //no games found checking steam api
   return await Axios.get(getGamesSteamUrl, {
     params: {
       key: apiKey,
@@ -65,6 +65,23 @@ const checkUserGames = async (steamId: string) => {
     .catch(err => console.log(err));
 };
 
+const getUsers = async (steamIds: string[]) => {
+  const partyPoopers: { [id: string]: { name: string } } = {};
+  const enrichedUserData: { [id: string]: { name: string } } = {};
+  for (const steamId of steamIds) {
+    const userData = await getUser(steamId);
+    const userGames = await checkUserGames(steamId);
+    if (!userGames) {
+      //party pooper found!!!1!
+      partyPoopers[steamId] = { name: userData.name };
+    } else {
+      enrichedUserData[steamId] = { name: userData.name };
+      // enrichedUserData[steamId].games = userGames; todo is this needed?
+    }
+  }
+  return [partyPoopers, enrichedUserData];
+};
+
 const getGameCategories = async (appId: string) => {
   const details = await getGameCategoriesFromDB(appId);
   if (!details) {
@@ -90,87 +107,53 @@ const getGameCategories = async (appId: string) => {
   return details;
 };
 
-const main = async (steamIds: string[]) => {
-  const partyPoopers: { [id: string]: { name: string } } = {};
-  const enrichedUserData: { [id: string]: { name: string } } = {};
-  for (const steamId of steamIds) {
-    const userData = await getUser(steamId);
-    const userGames = await checkUserGames(steamId);
-    if (!userGames) {
-      //party pooper found!!!1!
-      partyPoopers[steamId] = { name: userData.name };
-    } else {
-      enrichedUserData[steamId] = { name: userData.name };
-      // enrichedUserData[steamId].games = userGames;
+const filterGamesByMultiplayer = async (commonGames: any) => {
+  const commonMultiplayerGames = [];
+  //todo maybe chunk requests into (100?) batches with a wait after each?
+  for (const game of commonGames) {
+    const categories: any[] = await getGameCategories(game.appId);
+    //if categories contains one of the multiplayer categories add to list
+    if (
+      categories
+        .map(category => category.id)
+        .some(category =>
+          multiPlayerCategories
+            .map(multiPlayerCategory => multiPlayerCategory.id)
+            .includes(category)
+        )
+    ) {
+      commonMultiplayerGames.push(game);
     }
   }
+
+  return commonMultiplayerGames;
+};
+
+const main = async (steamIds: string[]) => {
+  const [partyPoopers, players] = await getUsers(steamIds);
 
   if (Object.keys(partyPoopers).length > 0) {
     console.log("party poopers found:", partyPoopers);
   }
 
-  if (Object.keys(enrichedUserData).length > 0) {
-    console.log("found games for these users:", enrichedUserData);
+  if (Object.keys(players).length > 0) {
+    console.log("found games for these users:", players);
   }
 
-  if (Object.keys(enrichedUserData).length > 1) {
+  if (Object.keys(players).length > 1) {
     console.log("attempting to find common games for users...");
-    //todo find common games, filter by category
-    const commonGames = await getGamesInCommonFromDB(
-      Object.keys(enrichedUserData)
-    );
+    const commonGames = await getGamesInCommonFromDB(Object.keys(players));
     if (commonGames.length > 0) {
       console.log(commonGames.length, "common games found:", commonGames);
       console.log("attempting to filter common games to multiplayer ones...");
-      const commonMultiplayerGames = [];
-      //todo maybe chunk requests into (100?) batches with a wait after each?
-      for (const game of commonGames) {
-        const categories: any[] = await getGameCategories(game.appId);
-        //todo filter by categories
-        const multiPlayerCategories = [
-          {
-            id: 1,
-            description: "Multi-player"
-          },
-          {
-            id: 9,
-            description: "Co-op"
-          },
-          {
-            id: 38,
-            description: "Online Co-op"
-          },
-          {
-            id: 44,
-            description: "Remote Play Together"
-          },
-          {
-            id: 39,
-            description: "Shared/Split Screen Co-op"
-          },
-          {
-            id: 24,
-            description: "Shared/Split Screen"
-          }
-        ];
-        //if categories contains one of these ^ add to list
-        if (
-          categories
-            .map(category => category.id)
-            .some(category =>
-              multiPlayerCategories
-                .map(multiPlayerCategory => multiPlayerCategory.id)
-                .includes(category)
-            )
-        ) {
-          commonMultiplayerGames.push(game);
-        }
-      }
+      const commonMultiplayerGames = await filterGamesByMultiplayer(
+        commonGames
+      );
       if (commonMultiplayerGames.length > 0) {
         console.log(
           commonMultiplayerGames.length,
           "common multi-player games found for:",
-          Object.values(enrichedUserData).map(user => user.name),
+          Object.values(players).map(user => user.name),
           commonMultiplayerGames
         );
         if (Object.keys(partyPoopers).length > 0) {
@@ -197,15 +180,15 @@ const main = async (steamIds: string[]) => {
 };
 
 main([
-  "76561197962348172", // BlueNovember
-  "76561197963604152", // Sarah
-  // "76561197991832554", // Dan
-  "76561198005421655", // Eagle
+  // "76561197962348172", // BlueNovember
+  // "76561197963604152", // Sarah
+  "76561197991832554", // Dan
+  // "76561198005421655", // Eagle
   // "76561198009777530", // Rob
-  "76561198026808627", // Crux
-  // "76561198040733783", // AggrievedSpark
+  // "76561198026808627", // Crux
+  "76561198040733783", // AggrievedSpark
   "76561198056906429", // Matthew
   // "76561198075241793", // David
-  "76561198075305674" // Shaun
-  // "76561198096214189"  // Shelby
+  // "76561198075305674" // Shaun
+  "76561198096214189" // Shelby
 ]);
