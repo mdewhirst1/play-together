ec6850671c4f0235283d3bc98bae0e66f6ff3bb4
Matthew Dewhirst <matthew.dewhirst@skybettingandgaming.com>
2021-02-05 17:29:24 +0000

    [PT-02] wip updating db to have seperate category table to improve query capabilities

---
+++ src/data-helpers/category-helper.ts
@@ -0,0 +1,22 @@
+import { closeDBConnection, connectToDB } from "./generic-helper";
+import { Components } from "../types";
+import Category = Components.Schemas.Category;
+
+export const addCategoryToDB = (categories: Category[]) => {
+  return new Promise((resolve, reject) => {
+    const db = connectToDB();
+    categories.forEach(category => {
+      db.run(
+        "INSERT OR IGNORE INTO categories (id, description) VALUES (?, ?)",
+        category,
+        (err: any) => {
+          if (err) {
+            reject(err);
+          }
+        }
+      );
+    });
+    resolve();
+    closeDBConnection(db);
+  });
+};
