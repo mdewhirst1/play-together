658990f3c3817d2e04ff93c49c378f4ebbbaa0a3
Matthew Dewhirst <matthew.dewhirst@skybettingandgaming.com>
2021-02-12 17:11:46 +0000

    [PT-02] created a single query to find games with multiplayer categories and fixed issue when checking if a game already had categories where the db queryr was allways returning a falsy value which would trigger a long call to the steam api

--- src/data-helpers/games-categories-helper.ts
+++ src/data-helpers/games-categories-helper.ts
@@ -1,7 +1,6 @@
 import { closeDBConnection, connectToDB } from "./generic-helper";
 import { Components } from "../types";
 import Category = Components.Schemas.Category;
-// import Game = Components.Schemas.Game;
 import { addCategoryToDB } from "./category-helper";
 
 export const addGameCategoriesToDB = async (
@@ -30,17 +29,42 @@ export const addGameCategoriesToDB = async (
     });
 };
 
-export const getGameCategoriesFromDB = (appId: string) => {
-  return new Promise<Category[]>((resolve, reject) => {
+export const checkGameCategoriesFromDB = (appId: string) => {
+  return new Promise((resolve, reject) => {
     const db = connectToDB();
     db.get(
-      "SELECT category.id, category.description FROM categories as category LEFT OUTER JOIN games_categories AS gc ON gc.categoryid == category.id WHERE ug.appId IS (?) ",
+      "SELECT category.id, category.description FROM categories as category LEFT OUTER JOIN games_categories AS gc ON gc.categoryid == category.id WHERE gc.appId IS (?) ",
       appId,
       (err, row) => {
         if (err) {
           reject(err);
         }
-        resolve(JSON.parse(row.categories));
+        resolve(!!row);
+      }
+    );
+    closeDBConnection(db);
+  });
+};
+
+export const getGamesWithCategoriesFromDB = (
+  appIds: string[],
+  categoryIds: number[]
+) => {
+  return new Promise<string[]>((resolve, reject) => {
+    const db = connectToDB();
+    db.all(
+      "SELECT DISTINCT game.appId FROM games_categories as game LEFT OUTER JOIN categories AS category ON game.categoryid == category.id WHERE game.appId IN (" +
+        appIds.map(() => "?").join(",") +
+        ") AND category.id IN (" +
+        categoryIds.map(() => "?").join(",") +
+        ") ",
+      [...appIds, ...categoryIds],
+      (err, rows) => {
+        if (err) {
+          reject(err);
+        }
+        const result = rows.map(row => row.appid.toString());
+        resolve(result);
       }
     );
     closeDBConnection(db);
--- src/data-helpers/games-helper.ts
+++ src/data-helpers/games-helper.ts
@@ -1,6 +1,5 @@
 import { closeDBConnection, connectToDB } from "./generic-helper";
 import { Components } from "../types";
-import Category = Components.Schemas.Category;
 
 //adds games to db if they are not already in there
 export const addGamesToDB = (games: Components.Schemas.Game[]) => {
@@ -17,45 +16,6 @@ export const addGamesToDB = (games: Components.Schemas.Game[]) => {
         }
       );
     });
-    resolve();
-    closeDBConnection(db);
-  });
-};
-
-//get game categories
-export const getGameCategoriesFromDB = (appId: string) => {
-  return new Promise<Category[]>((resolve, reject) => {
-    const db = connectToDB();
-    db.get(
-      "SELECT categories FROM games WHERE appId = (?)",
-      appId,
-      (err, row) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(JSON.parse(row.categories));
-      }
-    );
-    closeDBConnection(db);
-  });
-};
-
-export const UpdateGameInDB = (
-  appId: string,
-  name: string,
-  categories: string
-) => {
-  return new Promise((resolve, reject) => {
-    const db = connectToDB();
-    db.run(
-      "INSERT OR REPLACE INTO games (appId, name, categories) VALUES (?, ?, ?)",
-      [appId, name, categories],
-      (err: any) => {
-        if (err) {
-          reject(err);
-        }
-      }
-    );
     resolve();
     closeDBConnection(db);
   });
